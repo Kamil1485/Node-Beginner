3fa3634d9c731882db34947387692b59bfdfe5f7
Kamil1485 <81561240+Kamil1485@users.noreply.github.com>
2023-04-25 11:15:35 +0300

    Update modules.js

--- Fundamentals/Modules/modules.js
+++ Fundamentals/Modules/modules.js
@@ -2,7 +2,6 @@ const persons=["ahmet","mehmet","veli"]
 const animals=["dog","cat","lion"]
 
 //module.exports=persons;
-//her iki değişken için obje içinde exports yapılmalı
 //exports.animals=animals;
 
 module.exports={
